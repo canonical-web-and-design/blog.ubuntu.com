a0e7d4a9aca1ce9e78537ec72a5f430677869ce4
Anthony Dillon <me@anthonydillon.com>
2017-09-25 16:21:33 +0100

    Add basic template

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return flask.render_template('index.html')
