7b19ce0c657a8a218c27f42d6929a773986a4820
Anthony Dillon <me@anthonydillon.com>
2017-09-26 21:52:09 +0100

    Remove hardcoded URL routes

--- app.py
+++ app.py
@@ -86,34 +86,3 @@ def user(slug):
     data = json.loads(response.text)[0]
     data = _normalise_user(data)
     return flask.render_template('author.html', author=data)
-
-
-@app.route('/desktop/')
-def index_dev():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_path = os.path.join(SITE_ROOT, "data", "posts.json")
-    with open(json_path) as json_data:
-        data = json.load(json_data)
-    for post in data:
-        post = _normalise_post(post)
-    return flask.render_template('index.html', posts=data)
-
-
-@app.route('/2017/09/19/results-of-the-ubuntu-desktop-applications-survey/')
-def post_dev():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_path = os.path.join(SITE_ROOT, "data", "post.json")
-    with open(json_path) as json_data:
-        data = json.load(json_data)
-    data = _normalise_post(data)
-    return flask.render_template('post.html', post=data)
-
-
-@app.route('/author/dustin-kirkland-2/')
-def user_dev():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_path = os.path.join(SITE_ROOT, "data", "users.json")
-    with open(json_path) as json_data:
-        data = json.load(json_data)
-    data = _normalise_user(data)
-    return flask.render_template('author.html', author=data)
