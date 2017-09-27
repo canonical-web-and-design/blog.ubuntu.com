645008f7c52d5c6272e189a3ada796ac7e6b50e5
Will Moggridge <will@moggridge.com>
2017-09-27 14:28:11 -0400

    Caching!

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import datetime
 import flask
 import json
-import os
 import requests
-
+import requests_cache
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 from urllib.parse import urlsplit
 from werkzeug.routing import BaseConverter
 
@@ -11,6 +13,72 @@ INSIGHTS_URL = 'https://insights.ubuntu.com/wp-json/wp/v2'
 
 
 app = flask.Flask(__name__)
+
+
+# Setup session to retry requests 5 times
+uncached_session = requests.Session()
+retries = Retry(
+    total=5,
+    backoff_factor=0.1,
+    status_forcelist=[500, 502, 503, 504]
+)
+uncached_session.mount(
+    'https://api.snapcraft.io',
+    HTTPAdapter(max_retries=retries)
+)
+
+# The cache expires after 10 minutes
+cached_session = requests_cache.CachedSession(expire_after=600)
+
+# Requests should timeout after 2 seconds in total
+request_timeout = 10
+
+
+def _get_from_cache(url, json=None):
+    """
+    Retrieve the response from the requests cache.
+    If the cache has expired then it will attempt to update the cache.
+    If it gets an error, it will use the cached response, if it exists.
+    """
+
+    request_error = False
+
+    method = "POST" if json else "GET"
+
+    request = cached_session.prepare_request(
+        requests.Request(
+            method=method,
+            url=url,
+            json=json
+        )
+    )
+
+    cache_key = cached_session.cache.create_key(request)
+    response, timestamp = cached_session.cache.get_response_and_time(
+        cache_key
+    )
+
+    if response:
+        age = datetime.datetime.utcnow() - timestamp
+
+        if age > cached_session._cache_expire_after:
+            try:
+                new_response = uncached_session.send(
+                    request,
+                    timeout=request_timeout
+                )
+                if response.status_code >= 500:
+                    new_response.raise_for_status()
+            except:
+                request_error = True
+            else:
+                response = new_response
+    else:
+        response = cached_session.send(request)
+
+    response.old_data_from_error = request_error
+
+    return response
 
 
 class RegexConverter(BaseConverter):
@@ -38,7 +106,7 @@ def _get_posts(categories=[], tags=[], limit=10):
             tags = ','.join(str(tag) for tag in tags)
         ''.join([api_url, '&tags=', tags])
 
-    response = requests.get(api_url)
+    response = _get_from_cache(api_url)
     posts = json.loads(response.text)
     for post in posts:
         post = _normalise_post(post)
@@ -57,7 +125,7 @@ def _get_user_recent_posts(user_id, limit=5):
         user_id=user_id,
         limit=limit,
     )
-    response = requests.get(api_url)
+    response = _get_from_cache(api_url)
     posts = json.loads(response.text)
     for post in posts:
         post = _normalise_post(post)
@@ -103,7 +171,7 @@ def category(category):
 @app.route('/<regex("[0-9]{4}"):year>/<regex("[0-9]{2}"):month>/<regex("[0-9]{2}"):day>/<slug>/')
 def post(year, month, day, slug):
     api_url = ''.join([INSIGHTS_URL, '/posts?_embed&slug=', slug])
-    response = requests.get(api_url)
+    response = _get_from_cache(api_url)
     data = json.loads(response.text)[0]
     data = _normalise_post(data)
     data['related_posts'] = _get_related_posts(data)
@@ -113,7 +181,7 @@ def post(year, month, day, slug):
 @app.route('/author/<slug>/')
 def user(slug):
     api_url = ''.join([INSIGHTS_URL, '/users?_embed&slug=', slug])
-    response = requests.get(api_url)
+    response = _get_from_cache(api_url)
     data = json.loads(response.text)[0]
     data = _normalise_user(data)
     return flask.render_template('author.html', author=data)
