2a41804b033a3885a90fb311a6d872fcf4b3445e
Robin Winslow <robin@robinwinslow.co.uk>
2018-02-01 10:26:05 +0000

    Flake8 code style fixes

--- app.py
+++ app.py
@@ -159,17 +159,22 @@ def tag_index(slug):
             '404.html'
         )
 
+
 @app.route('/archives/<regex("[0-9]{4}"):year>/')
 def archives_year(year):
     result = api.get_archives(year)
     return flask.render_template('archives.html', result=result, today=datetime.utcnow())
+
 
 @app.route('/archives/<regex("[0-9]{4}"):year>/<regex("[0-9]{2}"):month>/')
 def archives_year_month(year, month):
     result = api.get_archives(year, month)
     return flask.render_template('archives.html', result=result, today=datetime.utcnow())
 
-@app.route('/archives/<group>/<regex("[0-9]{4}"):year>/<regex("[0-9]{2}"):month>/')
+
+@app.route(
+    '/archives/<group>/<regex("[0-9]{4}"):year>/<regex("[0-9]{2}"):month>/'
+)
 def archives_group_year_month(group, year, month):
     group_id = ''
     groups = []
@@ -221,6 +226,7 @@ def archives_group_year_month(group, year, month):
 
     result = api.get_archives(year, month, group_id, group_name)
     return flask.render_template('archives.html', result=result)
+
 
 @app.route(
     '/<regex("[0-9]{4}"):year>'
