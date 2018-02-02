bbfb4a1d8698cf39f19910aa8d0931c03e4867ec
Peter Mahnke <peter@transitionelement.com>
2018-02-02 11:21:37 +0000

    Updated press-centre right rail (#158)
    ## Done
    
    - Updated press-centre right rail
    - Added yearly archive
    - Added group archive to the archive pages (with only years)
    - Updated social list
    - Remove juju logos
    
    ## QA
    
    - Check out this feature branch
    - Run the site using the command `./run serve`
    - View the site locally in your web browser at: [press-centre](http://0.0.0.0:8023/press-centre/)
    - See that it matches the [copy doc]() and the archives work (lots of missing archives pre-2015!!!
    
    ## Issue / Card
    
    Fixes #146
    
    Signed-off-by: Peter Mahnke <peter@transitionelement.com>

--- app.py
+++ app.py
@@ -96,7 +96,8 @@ def group_category(group_slug, category_slug='all'):
             posts=posts,
             group=group,
             group_details=group_details,
-            category=category_slug if category_slug else None,
+            category=category if category else None,
+            today=datetime.utcnow(),
             **metadata
         )
     else:
@@ -172,26 +173,24 @@ def archives_year_month(year, month):
 
 @app.route('/archives/<group>/<regex("[0-9]{4}"):year>/')
 def archives_group_year(group, year):
-    group_id = ''
-    groups = []
+    group_slug = group
 
     if group == 'press-centre':
         group = 'canonical-announcements'
 
-    groups = local_data.get_group_by_slug(group)
+    groups = api.get_group_by_slug(group)
+
+    if not groups:
+        flask.abort(404)
+
     group_id = int(groups['id']) if groups else None
     group_name = groups['name'] if groups else None
 
-    if not group_id:
-        return flask.render_template(
-            '404.html'
-        )
-
     result = api.get_archives(year, None, group_id, group_name)
-
     return flask.render_template(
         'archives.html',
         result=result,
+        group=group_slug,
         today=datetime.utcnow(),
     )
 
