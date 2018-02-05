905c5be88d487280d1296d128557155578e68374
Robin Winslow <robin@robinwinslow.co.uk>
2018-02-05 18:07:29 +0000

    Add more URLs to test, and test for 404s

--- tests.py
+++ tests.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python3
-
 # Core
 import unittest
 import time
@@ -11,18 +9,50 @@ from api import get
 from helpers import ignore_warnings
 
 
-urls_to_test = [
+test_content = 'Ubuntu and Canonical are registered'
+
+working_uris = [
     '/',  # Homepage
-    '/cloud-and-server',  # Group page
+    '/cloud-and-server?page=2',  # Group page
     '/articles',  # Category page
-    '/cloud-and-server/case-studies',  # Group & category page
+    '/cloud-and-server/case-studies?page=2',  # Group & category page
     '/press-centre',  # Press centre
-    '/topics/maas',  # Topic page
+    '/topics/maas?page=2',  # Topic page
     '/author/canonical',  # Author page
     '/search',  # Search (empty)
     '/search?q=lxd',  # Search for a term
     '/tag/security',  # Tag page
+    '/archives/2018?page=2',  # Archives by year
+    '/archives/2018/02',  # Archives by year and month
+    '/archives/2018/01?page=2',  # Archives by year and month
+    '/archives/2099',  # Empty archive year
+    '/archives/2099/12',  # Empty archive month
+    '/archives/cloud-and-server/2099',  # Empty group archive year
+    '/archives/press-centre/2018?page=2',  # Press centre archive
+    '/archives/cloud-and-server/2018?page=2',  # Group page archive by year
     '/2018/01/24/meltdown-spectre-and-ubuntu-what-you-need-to-know',  # article
+]
+
+missing_uris = [
+    '/non-existent-group',  # Unknown group
+    '/𝐢𝜛𝒔𝞪𝓃ҽ-𝘶𝔯l',  # Weird UTF-8 chars in URL
+    '/2018', '/2018/01', '/2018/01/24',  # Date URLs
+    '/cloud-and-server/non-existent-category',  # Unknown category page
+    '/press-centre/2099',  # Press centre, erroneous year
+    '/press-centre/fake-url',  # Press centre, fake suffix
+    '/topics/non-existent',  # Missing topic
+    '/author',  # No author selected
+    '/author/non-existent-author',  # Missing author
+    '/search/fake-suffix?q=nothing',  # Search, fake suffix
+    '/tag',  # No tag selected
+    '/tag/fake-tag',  # Non-existent tag
+    '/archives',  # No selected archive
+    '/archives/201',  # Archive, non-year suffix
+    '/archives/2018/24',  # Non existent archive month
+    '/archives/cloud-and-server',  # Group archive, no selected year
+    '/archives/cloud-and-server/2018/02?page=2',  # Group archive, year + month
+    '/archives/non-existent-group/2018',  # Non existent group archive
+    '/2018/01/24/a-missing-article',  # article
 ]
 
 
@@ -58,16 +88,31 @@ class WebAppTestCase(unittest.TestCase):
         assert initial_posts == subsequent_posts
         assert request_time < 0.1
 
-    def test_urls(self):
+    @ignore_warnings(ResourceWarning)
+    def test_working_urls(self):
         """
         Check a bunch of URLs for basic success
         """
 
-        print(" Testing URLs:")
+        print(" Testing working URLs:")
 
-        for url in urls_to_test:
-            print('  - ' + url + ' ... ', end='')
-            self._check_basic_page(url)
+        for uri in working_uris:
+            print('  - ' + uri + ' ... ', end='')
+            self._check_basic_page(uri)
+            print('done')
+
+    def test_missing_urls(self):
+        """
+        Check a bunch of URLs for basic success
+        """
+
+        print(" Testing missing URLs:")
+
+        for uri in missing_uris:
+            print('  - ' + uri + ' ... ', end='')
+            response = self.app.get(uri)
+            assert response.status_code == 404
+            assert test_content in str(response.data)
             print('done')
 
     def _get_check_slash_normalisation(self, uri):
@@ -119,7 +164,7 @@ class WebAppTestCase(unittest.TestCase):
             response = self._get_check_slash_normalisation(uri)
 
         assert response.status_code == 200
-        assert "Ubuntu and Canonical are registered" in str(response.data)
+        assert test_content in str(response.data)
 
         return response
 
