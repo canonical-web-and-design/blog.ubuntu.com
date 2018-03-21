4338917115e00d44caa2c0d2ff98dd037874937d
Steve Rydz <steverydz@gmail.com>
2018-03-21 12:11:35 +0000

    Fix search reset button

---
+++ static/js/search.js
@@ -0,0 +1,19 @@
+(function() {
+  function handleSearch(searchBox) {
+    let searchBoxes = Array.prototype.slice.call(
+      document.querySelectorAll(searchBox)
+    );
+
+    searchBoxes.forEach(searchBox => {
+      let searchBoxInput = searchBox.querySelector('.p-search-box__input');
+      let searchBoxResetBtn = searchBox.querySelector('.p-search-box__reset');
+
+      searchBoxResetBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        searchBoxInput.value = '';
+      }, false);
+    });
+  }
+
+  handleSearch('.p-search-box');
+})();
